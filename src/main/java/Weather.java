84dec5a475e5eb862cc5e350776c7e6213b896a8
JUAN E TENORIO ARZOLA <tenoriojuann@gmail.com>
2016-11-10 14:39:50 -0500

    NEW CALENDAR

--- src/main/java/Weather.java
+++ src/main/java/Weather.java
@@ -2,16 +2,25 @@
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
 
 public class Weather {
 
-    private String key = "0542a8054137881685a1941c9de05d2a";
+
+    //OPENWEATHER KEY
+    private InputStream sc = this.getClass().getResourceAsStream("/key.json");
+
+    private JsonParser parser = new JsonParser();
+    private JsonObject JSON = (JsonObject) parser.parse(new InputStreamReader(
+                                                                sc));
 
     private String lat,lng;
 
@@ -33,29 +42,29 @@ public class Weather {
         HttpURLConnection requestLocation = (HttpURLConnection) ipURL.openConnection();
         requestLocation.connect();
 
-        JsonParser IP = new JsonParser();
 
-        JsonElement root2 = IP.parse(new InputStreamReader((InputStream) requestLocation.getContent()));
+
+        JsonElement root2 = parser.parse(new InputStreamReader((InputStream) requestLocation.getContent()));
 
         location = root2.getAsJsonObject();
 
         setLocation();
 
-        URL url = new URL("http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lng+"&units=imperial&appid="+key);
+        URL url = new URL("http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lng+"&units=imperial&appid="+JSON.get("WeatherAPIKey").getAsString());
 
         HttpURLConnection request = (HttpURLConnection) url.openConnection();
         request.connect();
 
-        JsonParser jp = new JsonParser(); //from gson
-        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
+
+        JsonElement root = parser.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
         data = root.getAsJsonObject(); //May be an array, may be an object.
 
     }
 
 
     public void setLocation(){
-        lat = location.get("lat").toString();
-        lng = location.get("lon").toString();
+        lat = location.get("lat").getAsString();
+        lng = location.get("lon").getAsString();
     }
 
     public String getLocation(){
@@ -66,12 +75,12 @@ public class Weather {
     public String getTemp(){
         JsonObject main = data.get("list").getAsJsonArray().get(0).getAsJsonObject().get("main").getAsJsonObject();
 
-        return main.get("temp").toString();
+        return main.get("temp").getAsString();
     }
 
     public String getForecast(){
         JsonObject weather = data.get("list").getAsJsonArray().get(0).getAsJsonObject().get("weather").getAsJsonArray().get(0).getAsJsonObject();
 
-        return weather.get("description").toString();
+        return weather.get("description").getAsString();
     }
 }
