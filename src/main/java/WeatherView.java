26e2c7b31a0006d9d18baf8fbcb66fc6c50919fc
Daniel Park <dpark@spsu.edu>
2016-11-14 15:08:26 -0500

    Added class to display weather

---
+++ src/main/java/WeatherView.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Created by La molybdenstar on 11/14/2016.
+ */
+public class WeatherView extends Application{
+
+    Weather W = new Weather();
+
+    public WeatherView() throws IOException {
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
