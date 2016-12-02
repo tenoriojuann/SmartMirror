0872b75da3101d2042c40c76d85163475757cc14
JUAN E TENORIO ARZOLA <tenoriojuann@gmail.com>
2016-12-02 15:38:52 -0500

    almost done

--- src/main/java/WeatherView.java
+++ src/main/java/WeatherView.java
@@ -1,20 +1,25 @@
 import com.sun.javafx.css.Style;
 import javafx.application.Application;
+import javafx.event.Event;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.*;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.*;
-import javafx.scene.shape.Shape;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-import java.awt.*;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.EventListener;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by La molybdenstar on 11/14/2016.
@@ -32,15 +37,33 @@ import java.io.IOException;
  To pull the weather information we simply rewrote the Python script that we had for one of the assignments into Java and JavaFX.
  */
 public class WeatherView extends Application{
-<<<<<<< HEAD
 
 
-
-=======
-    
->>>>>>> origin/master
+    Timer timer = new Timer ();
 
     public WeatherView() throws IOException {
+
+    }
+
+    public Image chooseIcon(String text){
+
+        if(text.toLowerCase().contains("Cloud".toLowerCase())){
+            return new Image(this.getClass().getResource("/Icons/Clouds.png").toExternalForm());
+        }
+        else if(text.toLowerCase().contains("rain".toLowerCase())){
+            return new Image(this.getClass().getResource("/Icons/rain.png").toExternalForm());
+        }
+        else if(text.toLowerCase().contains("snow".toLowerCase())){
+            return new Image(this.getClass().getResource("/Icons/Snow.png").toExternalForm());
+        }
+        else if(text.toLowerCase().contains("Sun".toLowerCase()) || text.toLowerCase().contains("clear".toLowerCase())){
+            return new Image(this.getClass().getResource("/Icons/Sun.png").toExternalForm());
+        }
+        else if(text.toLowerCase().contains("heavy rain".toLowerCase())){
+            return new Image(this.getClass().getResource("/Icons/HeavyRain.png").toExternalForm());
+        }
+
+        return null;
 
     }
 
@@ -49,18 +72,51 @@ public class WeatherView extends Application{
 
         Weather W = new Weather();
         BorderPane pane = new BorderPane();
-        pane.setStyle("-fx-background: black;");
-        ScrollPane scroll = new ScrollPane();
-        pane.setCenter(scroll);
-        scroll.setStyle("-fx-background-color: black;");
+        ImageView image = new ImageView();
+
+        //pane.setBackground();
+        pane.setCenter(image);
 
 
-        Label label = new Label(W.getForecast()+"\n\n" + W.getLocation()+"\n\n"+ W.getTemp());
-        label.setTextFill(Color.CYAN);
-        label.setStyle("-fx-font-size: 25");
-        scroll.setContent(label);
+        Text text ;
+
+
+        text = new Text(W.getForecast());
+
+        text.setFill(Color.CYAN);
+        text.setStyle("-fx-font-size: 50pt ;");
+        pane.setBottom(text);
+
+        image.setImage(chooseIcon(text.getText()));
+
+
+        TimerTask hourlyTask = new TimerTask () {
+            @Override
+            public void run () {
+                text.setText(W.getForecast());
+            }
+        };
+
+        // schedule the task to run starting now and then every hour...
+        timer.schedule (hourlyTask, 0l, 1000*60*30);
+
+
+
+        // Action Listener, listening for changes in the text
+
+        text.textProperty().addListener((observable, oldValue, newValue) -> {
+            image.setImage(chooseIcon(newValue));
+        });
+
 
         Scene scene = new Scene(pane);
+
+
+        // Transparent
+        scene.setFill(null);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        // Transparent
+
 
         primaryStage.setScene(scene);
         primaryStage.show();
