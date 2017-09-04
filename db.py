96fd654aef28682904e8a5968a118170c03bebab
Juan E. Tenorio Arzola <tenoriojuann@gmail.com>
2017-09-03 23:07:26 -0400

    added comments

--- db.py
+++ db.py
@@ -4,21 +4,26 @@ import os
 
 APP_ROOT = os.path.dirname(os.path.dirname(__file__))
 
+# Database class which will be used to connect or execute commands in the DB
 
 class DB:
 
+# Initializing the class with some class variables
     def __init__(self):
         self.version = sqlite3.version
         self.conn = sqlite3.connect(os.path.join(APP_ROOT, "db.db"))
         print("DB has been opened")
         self.cursor = self.conn.cursor()
 
+    # Method that can be called to execute a command
     def executeSQL(self, command, values):
         return self.cursor.executemany(command, values).fetchall()
 
     def displayVersion(self):
         print("DB version: ", self.version)
 
+    # Adds a profile to the DB if it does not exists
+    # It should make sure to convert 'preferences' to a string and store it or make new columns in DB
     def addProfile(self, name, email, token, preferences):
         userEmail = "select * from USERS where EMAIL= ?"
         if len(self.executeSQL(userEmail, email)) == 0:
@@ -30,4 +35,6 @@ class DB:
             self.conn.commit()
             print("A new record was be added")
         else:
+            # We need to return something to the API end point here so it can display a
+            # message in the UI
             print("A record with that email has already been registered")
