49222867007fc4ec458d4bbdceb776e7a37daf18
Juan Tenorio Arzola <jtenorio@lat-rkhakzad1.cb.careerbuilder.com>
2017-09-08 16:16:05 -0400

    added a model class for an user

---
+++ static/User.py
@@ -0,0 +1,36 @@
+class User:
+
+    def __init__(self, name, email, googletoken, pin):
+        self.name = name
+        self.email = email
+        self.googletoken = googletoken
+        self.spotifytoken = ""
+        self.facepath = ""
+        self.pin = pin
+        self.twittertoken = ""
+
+
+    def setName(self, name):
+        self.name = name
+
+    def setEmail(self, email):
+        self.email = email
+
+    def setGoogleToken(self, token):
+        self.googletoken = token
+
+    def setSpotifyToken(self, token):
+        self.spotifytoken = token
+
+    def setFacePath(self, path):
+        self.facepath = path
+
+    def setPin(self, pin):
+        self.pin = self.encode(pin)
+
+    def encode(self, pin):
+        # TODO: make an encoding algorithm
+        return pin
+
+    def setTwitterToken(self, token):
+        self.twittertoken = token
