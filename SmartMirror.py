0656548c5a820d28f0747bb9024dbc68b5870543
Juan E. Tenorio Arzola <tenoriojuann@gmail.com>
2017-09-14 22:58:02 -0400

    /register endpoint mostly done, only thing left is to decide what information from google we want to show

--- SmartMirror.py
+++ SmartMirror.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 import db
-from werkzeug.exceptions import HTTPException, BadRequest
+from werkzeug.exceptions import BadRequest
 app = Flask(__name__, static_folder="static", static_url_path="/static",
             template_folder="templates")
 database = db.DB(app.root_path)
@@ -13,18 +13,13 @@ def index():
 
 @app.route('/register', methods=['POST'])
 def getPreferences():
-    if request.method == 'POST':
-        content = request.get_json(force=True)
-        print(content)
-        try:
-            database.addProfile(content)
-        except BadRequest as e:
-            return e
-        return render_template('register.html')
-    else:
-        return """<html><body>
-        A record with that email has already been set up.
-        </body></html>"""
+    content = request.get_json(force=True)
+    print(content)
+    try:
+        database.addProfile(content)
+    except BadRequest as e:
+        return e
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run()
--- db.py
+++ db.py
@@ -2,28 +2,28 @@
 import sqlite3
 from static import User
 from werkzeug.exceptions import BadRequest
+
+
 # Database class which will be used to connect or execute commands in the DB
 
 
 class DB:
-
-# Initializing the class with some class variables
+    # Initializing the class with some class variables
     def __init__(self, root):
         self.root = root
         self.version = sqlite3.version
-        self.conn = sqlite3.connect(root+"\DATA.db")
-        print(root+"\DATA.db")
+        self.conn = sqlite3.connect(root + "\DATA.db")
+        print(root + "\DATA.db")
         print("DB has been opened")
         self.cursor = self.conn.cursor()
 
     def displayVersion(self):
         print("DB version: ", self.version)
 
-
     def insertUserData(self, user):
-        add = "insert into USERS (name, email, googletoken, pin, facepath, spotifytoken, twittertoken) VALUES (?,?,?,?,?,?,?)"
+        add = "INSERT INTO USERS (name, email, googletoken, pin, facepath, spotifytoken, twittertoken) VALUES (?,?,?,?,?,?,?)"
         self.conn.executemany(add, [(user.name, user.email, user.googletoken, user.pin,
-                              user.facepath, user.spotifytoken,user.twittertoken)])
+                                     user.facepath, user.spotifytoken, user.twittertoken)])
         self.conn.commit()
 
     # Adds a profile to the DB if it does not exists
@@ -32,25 +32,31 @@ class DB:
             user = self.createUser(content)
         except BadRequest as e:
             print(e.description)
-            raise BadRequest
-        userByEmail = 'SELECT COUNT(*) from USERS where EMAIL= ?'
-        count = self.conn.execute(userByEmail, [user.email]).fetchone()[0]
-        if count == 0:
-            print("No record found with that email")
+            raise e
+
+        if not self.isUserRegistered(user):
             self.insertUserData(user)
             print("A new record was be added")
         else:
             # We need to return something to the API end point here so it can display a
             # message in the UI
-            print("A record with that email has already been registered")
-            raise sqlite3.DataError
+            raise BadRequest("A record with that email has already been registered")
 
-    def createUser(self, content):
+    def isUserRegistered(self, user):
+
+        userByEmail = 'SELECT COUNT(*) FROM USERS WHERE EMAIL= ?'
+        count = self.conn.execute(userByEmail, [user.email]).fetchone()[0]
+        if count > 0:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def createUser(content):
         try:
             user = User.User(content["name"], content["email"], content["googletoken"], content["pin"])
         except KeyError as e:
-            print("I/O error: {0} was not included".format(e))
-            raise BadRequest
+            raise BadRequest("I/O error: {0} was not included".format(e))
         try:
             user.setSpotifyToken(content["spotifytoken"])
         except KeyError as e:
