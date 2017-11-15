b30ee4dc086d611e49dcd56daafb261ef4b7e98f
J-Sexton <josh.b.sexton@gmail.com>
2017-11-14 19:54:05 -0500

    new shit

--- facialAuth.py
+++ facialAuth.py
@@ -114,9 +114,7 @@ def facial_authenticate(image):
         # Grab a single frame of video
         cv2.imwrite("test2.jpg", image)
         # Resize frame of video to 1/4 size for faster face recognition processing
-
-        #small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-        # Only process every other frame of video to save time
+        small_frame = cv2.resize(image, (0, 0), fx=0.25, fy=0.25)
         try:
             if process_this_frame:
                 user_face_dict = get_all_email_images()
@@ -124,12 +122,9 @@ def facial_authenticate(image):
                     user_image = face_recognition.load_image_file(key + "/" + key + ".jpg")
                     user_face_encoding = face_recognition.face_encodings(user_image)[0]
 
-                    face_locations = face_recognition.face_locations(image)
-                    face_encodings = face_recognition.face_encodings(image, face_locations)
-                    face_locations = []
-                    face_names = []
+                    face_locations = face_recognition.face_locations(small_frame)
+                    face_encodings = face_recognition.face_encodings(small_frame, face_locations)
                     for face_encoding in face_encodings:
-                        # See if the face is a match for the known face(s)
                         match = face_recognition.compare_faces([user_face_encoding], face_encoding)
                         status = "Unknown"
 
@@ -138,7 +133,6 @@ def facial_authenticate(image):
                             print(status + " : "+ key)
                             webbrowser.open_new_tab("http://172.20.10.8:5000/mirror/"+key)
                             return True
-                    #break
                         else:
                             print(status)
                             break;
@@ -149,33 +143,6 @@ def facial_authenticate(image):
         except:
             e = sys.exc_info()[0]
             print("<p>Error: %s</p>" % e)
-
-        # # Display the results
-        # for (top, right, bottom, left), name in zip(face_locations, face_names):
-        #     # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        #     top *= 4
-        #     right *= 4
-        #     bottom *= 4
-        #     left *= 4
-        #
-        #     # Draw a box around the face
-        #     cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-        #
-        #     # Draw a label with a name below the face
-        #     cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        #     font = cv2.FONT_HERSHEY_DUPLEX
-        #     cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-        #
-        # # Display the resulting image
-        # cv2.imshow('Video', frame)
-
-        # Hit 'q' on the keyboard to quit!
-        #if cv2.waitKey(1) & 0xFF == ord('q'):
-        #   break
-
-    # Release handle to the webcam
-    ##video_capture.release()
-    ##cv2.destroyAllWindows()
     return False
 
 def captureImage(userName):
