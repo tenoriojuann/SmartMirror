71b17c27703587e6528007a6f12187305e01ffa7
J-Sexton <josh.b.sexton@gmail.com>
2017-11-16 16:14:21 -0500

    fixed facial authentication

--- facialAuth.py
+++ facialAuth.py
@@ -26,6 +26,8 @@ from db import DB
 #provide name of image
 database = DB("/Users/jsexton/Senior")
 process_this_frame= True
+detect_motion = True
+is_user_viewing = False
 
 def get_all_email_images():
     emails = database.getEmail()
@@ -35,58 +37,60 @@ def get_all_email_images():
         user_image_dict[email[0]] = a_user_image
     return user_image_dict
 
-def diffImg(i1, i2, i3):
-    d1 = cv2.absdiff(i1,i2)
-    d2 = cv2.absdiff(i1,i3)
-    ret, thres = cv2.threshold(i1, 10, 0xff, cv2.THRESH_BINARY)
-    #print (d1)
-    #print (d2)
-    if ((d1 != d2).all()):
-        print("Movement!")
-    else:
-        print("No Movement!")
-    return cv2.bitwise_and(d1,d2)
 
 def monitor():
     movement_counter = 0
     cam = cv2.VideoCapture(0)
     test = cam.read()[1]
     firstFrame = None
-    while True:
+    while detect_motion:
         frame = cam.read()[1]
         frame = imutils.resize(frame, width=500)
-        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        gray = cv2.GaussianBlur(gray, (15,15), 0)
-        cv2.imwrite("grey_blur.jpg", gray)
+        #coversts to grey
+        currentFrame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        #Performs Gaussian Blure on image
+        currentFrame = cv2.GaussianBlur(currentFrame, (25,25), 25)
+        cv2.imwrite("grey_blur.jpg", currentFrame)
+        #If we have not established a first frame( the basis of all of our motion tracking) then we establish it here
         if firstFrame is None:
             print("Got first frame")
-            firstFrame = gray
+            #Save first frame
+            firstFrame = currentFrame
             cv2.imwrite("first.jpg", firstFrame)
+            #Starts over
             continue
-        frameDelta = cv2.absdiff(firstFrame, gray)
-        thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+        #Calculates Agsolute Difference between the first frame and the current frame
+        frameDelta = cv2.absdiff(firstFrame, currentFrame)
+        #Writes out frameDelta as jpg
+        cv2.imwrite("frameDelta.jpg", frameDelta)
+        #Does the threshold of the image(may need tweeking depending on environment)
+        thresh = cv2.threshold(frameDelta, 100, 255, cv2.THRESH_BINARY)[1]
         thresh = cv2.dilate(thresh, None, iterations=2)
+        #Writes out thresh image as thresh.jog
         cv2.imwrite("thresh.jpg", thresh)
+        #Finds contours within the thresh image
         (cnts, contours, _) = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         for c in contours:
+
             area = cv2.contourArea(c)
-            #print(area)
+            #If size of contour is not big enough, ignore it(aka, if the movement is not big enough ignore it)
             if cv2.contourArea(c) < 1000:
                 continue
+            #We look for three points of movement within any check
             else:
                 movement_counter+=1
                 print("Movement")
+                #If mwe detect 3 points of movement we begin facial authentication
                 if movement_counter % 3 == 0:
-                    ret, image = cam.read()
-                    if ret:
-                        cv2.imwrite("test1.jpg", image)
-                        fac = facial_authenticate(image)
-                        print (fac)
-                        if fac is True:
-                            print("Success!")
-                        else:
-                            continue
+                    #Frame represnets the current frame of the system.
+                    cv2.imwrite("test1.jpg", frame)
+                    facial_authentication_results = facial_authenticate(frame)
+                    print (facial_authentication_results)
+                    if facial_authentication_results is True:
+                        print("Success!")
+                    else:
+                        continue
 
    ## while True:
    ##     cv2.imshow(window, diffImg(image_minus, image_base, image_plus))
@@ -100,41 +104,37 @@ def monitor():
    ##         break
 
 def facial_authenticate(image):
-    process_this_frame = True
-    while process_this_frame:
-        # Grab a single frame of video
-        cv2.imwrite("test2.jpg", image)
-        # Resize frame of video to 1/4 size for faster face recognition processing
-        small_frame = cv2.resize(image, (0, 0), fx=0.25, fy=0.25)
-        try:
-            if process_this_frame:
-                user_face_dict = get_all_email_images()
-                for key in user_face_dict.keys():
-                    user_image = face_recognition.load_image_file(key + "/" + key + ".jpg")
-                    user_face_encoding = face_recognition.face_encodings(user_image)[0]
+    global detect_motion
+    cv2.imwrite("test2.jpg", image)
+    # Resize frame of video to 1/4 size for faster face recognition processing
+    small_frame = cv2.resize(image, (0, 0), fx=0.25, fy=0.25)
+    try:
+        user_face_dict = get_all_email_images()
+        for key in user_face_dict.keys():
+            user_image = face_recognition.load_image_file(key + "/" + key + ".jpg")
+            user_face_encoding = face_recognition.face_encodings(user_image)[0]
 
-                    face_locations = face_recognition.face_locations(small_frame)
-                    face_encodings = face_recognition.face_encodings(small_frame, face_locations)
-                    for face_encoding in face_encodings:
-                        match = face_recognition.compare_faces([user_face_encoding], face_encoding)
-                        status = "Unknown"
+            face_locations = face_recognition.face_locations(small_frame)
+            face_encodings = face_recognition.face_encodings(small_frame, face_locations)
+            for face_encoding in face_encodings:
+                match = face_recognition.compare_faces([user_face_encoding], face_encoding)
+                status = "Unknown"
 
-                        if match[0]:
-                            status = "Success"
-                            print(status + " : "+ key)
-                            webbrowser.open_new_tab("http://172.20.10.8:5000/mirror/"+key)
-                            return True
-                        else:
-                            print(status)
-                            break;
+                if match[0]:
+                    status = "Success"
+                    print(status + " : "+ key)
+                    webbrowser.open_new_tab("http://172.20.10.8:5000/mirror/"+key)
+                    detect_motion = False
+                    is_user_viewing = True
+                    return True
+                else:
+                    print(status)
+                    break;
 
-                        face_names.append(status)
-
-            process_this_frame = not process_this_frame
-        except:
-            e = sys.exc_info()[0]
-            print("<p>Error: %s</p>" % e)
-    return False
+                face_names.append(status)
+    except:
+        e = sys.exc_info()[0]
+        print("<p>Error: %s</p>" % e)
 
 def captureImage(userName):
     global process_this_frame
